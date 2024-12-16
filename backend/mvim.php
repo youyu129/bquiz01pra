8357a441aec272ec95df03dac5f63538f8a93f4d
youyu <129youyu@gmail.com>
2024-12-16 16:20:03 +0800

    backend/title.php複製為ad、image、mvim

--- backend/mvim.php
+++ backend/mvim.php
@@ -13,14 +13,15 @@
         </tbody>
     </table>
     <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-        <p class="t cent botli">動態文字廣告管理</p>
+        <p class="t cent botli">動畫圖片管理</p>
         <form method="post" action="api/edit">
             <table width="100%">
                 <tbody>
                     <tr class="yel">
-                        <td width="80%">動態文字廣告</td>
+                        <td width="45%">動畫圖片</td>
                         <td width="10%">顯示</td>
                         <td width="10%">刪除</td>
+                        <td width="35%"></td>
                         <td></td>
                     </tr>
                 </tbody>
@@ -30,7 +31,7 @@
                     <tr>
                         <td width="200px"><input type="button"
                                 onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;./modal/<?=$do;?>.php?table=<?=$do;?>&#39;)"
-                                value="新增動態文字廣告"></td>
+                                value="新增動畫圖片"></td>
                         <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置">
                         </td>
                     </tr>
