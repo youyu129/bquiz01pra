1ed735b6e502318da22bb7f9723d8dd2e0214dee
youyu <129youyu@gmail.com>
2024-12-16 11:56:15 +0800

    modal/title.php新增文字_新增form表單及table

--- modal/title.php
+++ modal/title.php
@@ -1,1 +1,26 @@
-123
+<h3 class="cent">
+    新增標題區圖片
+</h3>
+<hr>
+<form action="api/insert.php" method="post" enctype="multpart/form-data">
+
+    <table>
+        <tr>
+            <td>
+                標題區圖片：
+            </td>
+            <td>
+                <input type="file" name="img" id="img">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                標題區替代文字：
+            </td>
+            <td>
+                <input type="text" name="text" id="text">
+            </td>
+        </tr>
+    </table>
+
+</form>
