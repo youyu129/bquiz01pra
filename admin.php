472c0f89634b16d4abea1b79ca46e7bc8e4bc457
youyu <129youyu@gmail.com>
2024-12-16 10:36:36 +0800

    將admin的di區塊移至title_使用GET來載入檔案及頁面_include title_刪除admin的js

--- admin.php
+++ admin.php
@@ -31,8 +31,7 @@
                 <div id="menuput" class="dbor">
                     <!--主選單放此-->
                     <span class="t botli">後台管理選單</span>
-                    <a style="color:#000; font-size:13px; text-decoration:none;"
-                        href="./Management page_files/Management page.htm">
+                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=admin&redo=title">
                         <div class="mainmu">
                             網站標題管理 </div>
                     </a>
@@ -76,67 +75,15 @@
                         1 </span>
                 </div>
             </div>
-            <div class="di"
-                style="height:540px; border:#999 1px solid; width:76.5%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
-                <!--正中央-->
-                <table width="100%">
-                    <tbody>
-                        <tr>
-                            <td style="width:70%;font-weight:800; border:#333 1px solid; border-radius:3px;"
-                                class="cent"><a href="?do=admin" style="color:#000; text-decoration:none;">後台管理區</a>
-                            </td>
-                            <td><button onclick="document.cookie=&#39;user=&#39;;location.replace(&#39;?&#39;)"
-                                    style="width:99%; margin-right:2px; height:50px;">管理登出</button></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-                    <p class="t cent botli">網站標題管理</p>
-                    <form method="post" target="back" action="?do=tii">
-                        <table width="100%">
-                            <tbody>
-                                <tr class="yel">
-                                    <td width="45%">網站標題</td>
-                                    <td width="23%">替代文字</td>
-                                    <td width="7%">顯示</td>
-                                    <td width="7%">刪除</td>
-                                    <td></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <table style="margin-top:40px; width:70%;">
-                            <tbody>
-                                <tr>
-                                    <td width="200px"><input type="button"
-                                            onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;view.php?do=title&#39;)"
-                                            value="新增網站標題圖片"></td>
-                                    <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置">
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-
-                    </form>
-                </div>
-            </div>
+            <?php
+            $do=$_GET['do']??'title';
+            $file="./backend/{$do}.php";
+            include(file_exists($file))?$file:"./backend/title.php";
+            ?>
             <div id="alt"
                 style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;">
             </div>
-            <script>
-            $(".sswww").hover(
-                function() {
-                    $("#alt").html("" + $(this).children(".all").html() + "").css({
-                        "top": $(this).offset().top - 50
-                    })
-                    $("#alt").show()
-                }
-            )
-            $(".sswww").mouseout(
-                function() {
-                    $("#alt").hide()
-                }
-            )
-            </script>
+
         </div>
         <div style="clear:both;"></div>
         <div
